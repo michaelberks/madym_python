092356102dc7be08726e4f599f5d061c60790073
Michael Berks <michael.berks@manchester.ac.uk>
2021-06-10 15:28:02 +0100

    FIX(version): trying to get semantic-release to push version file back to repo

--- version.sh
+++ version.sh
@@ -3,6 +3,8 @@
 #This script is called by semantic-release to 
 #generate a new version file automatically in Gitlab CI pipeline
 echo "Writing version $1"
-echo "#This file is auto-generated in GitLab CI and should not be edited manually\n" > VERSION.txt
-echo $1 >> VERSION.txt
-ls
+echo "#This file is auto-generated in GitLab CI and should not be edited manually" > VERSION
+echo $1 >> VERSION
+git add VERSION
+git commit -m "Auto version update [ci skip]"
+git push
